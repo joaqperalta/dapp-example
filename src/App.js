b03389e88be7c17bc8d71e5f98b4fe026153a08f
joaqperalta <lahim.kane@outlook.com>
2021-10-25 00:29:38 +0300

    dashboard dompleted

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider, createTheme } from "@material-ui/core/styles";
 import { Container, Grid } from "@material-ui/core";
-import MetaLogIn from './login'
-import Dashboard from './dashboard'
+import MetaLogIn from "./login";
+import Dashboard from "./dashboard";
 
 const themeDark = createTheme({
   palette: {
@@ -22,14 +22,10 @@ const App = () => {
     <ThemeProvider theme={themeDark}>
       <CssBaseline />
       <Container fixed>
-        <Grid container>
-          <Grid item xs={12} sm={3} md={3}>
-            <MetaLogIn />
-          </Grid>
-          <Grid item xs={12} sm={9} md={9}>
-            <Dashboard />
-          </Grid>
+        <Grid container justifyContent="flex-end" >
+          <MetaLogIn />
         </Grid>
+        <Dashboard />
       </Container>
     </ThemeProvider>
   );
--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { Box } from "@material-ui/core"
+import { useEffect, useState } from "react";
 import {
   Table,
   TableBody,
@@ -8,64 +7,116 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Typography,
+  Grid,
+  Box,
 } from "@material-ui/core";
-import axios from 'axios'
+import { withStyles } from "@material-ui/core/styles";
+import { Pagination } from "@material-ui/lab";
+import axios from "axios";
+import Icon from "react-crypto-icons";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
+const NoBorderCell = withStyles({
+  root: {
+    borderBottom: "none",
+    textAlign: "center",
+  },
+})(TableCell);
 
 export default function Dashboard() {
   useEffect(() => {
-    // const pricesWs = new WebSocket('wss://ws.coincap.io/prices?assets=bitcoin,ethereum,monero,litecoin')
-    // pricesWs.onmessage = function (msg) {
-    //   console.log("data", msg.data)
-    // }
-    axios.get("https://api.coincap.io/v2/assets?offset=1&limit=10").
-    then(res => {
-      console.log("data", res.data)
-    })
-  }, [])
+    axios
+      .get("https://api.coincap.io/v2/assets?offset=0&limit=20")
+      .then((res) => {
+        console.log("data arrived", res.data.data);
+        setData(res.data.data);
+      })
+      .catch((err) => console.log("err occured:", err));
+  }, []);
+
+  const pricesWs = new WebSocket("wss://ws.coincap.io/prices?assets=ALL");
+  pricesWs.onmessage = function (msg) {
+    let modify = JSON.parse(msg.data);
+    let currencies = Object.keys(modify);
+    let res = [...data];
+    if (res.length > 0) {
+      currencies.forEach((curr) => {
+        res = res.map((item) =>
+          item.name.toLowerCase() == curr
+            ? { ...item, priceUsd: modify[curr] }
+            : item
+        );
+      });
+      setData(res);
+    }
+  };
+
+  const [page, setPage] = useState(0);
+  const [data, setData] = useState([]);
+
+  const handlePagination = (e, page) => {
+    axios
+      .get(
+        `https://api.coincap.io/v2/assets?offset=${(page - 1) * 20}&limit=20`
+      )
+      .then((res) => {
+        setData(res.data.data);
+      })
+      .catch((err) => console.log("err occured:", err));
+  };
 
   return (
-    <Box p={3}>
-      <TableContainer component={Paper} style={{backgroundColor: "#05182b"}} >
-        <Table sx={{ minWidth: 650 }} aria-label="simple table" >
-          <TableHead>
-            <TableRow>
-              <TableCell>#</TableCell>
-              <TableCell align="right" >Icon</TableCell>
-              <TableCell align="right" >Name</TableCell>
-              <TableCell align="right" >Price(USD)</TableCell>
-              <TableCell align="right" >Protein&nbsp;(g)</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <TableRow
-                key={row.name}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+    <Box p={3} pt={0}>
+      <Box textAlign="center" mb={2}>
+        <Typography variant="h3">Cryptocurrency Price</Typography>
+      </Box>
+      {data.length > 0 ? (
+        <TableContainer
+          component={Paper}
+          style={{ backgroundColor: "#05182b" }}
+        >
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <NoBorderCell>#</NoBorderCell>
+                <NoBorderCell align="right">Name</NoBorderCell>
+                <NoBorderCell align="right">Icon</NoBorderCell>
+                <NoBorderCell align="right">Symbol</NoBorderCell>
+                <NoBorderCell align="right">Price(USD)</NoBorderCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            </TableHead>
+            <TableBody>
+              {data.map((row, i) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <NoBorderCell component="th" scope="row">
+                    {page * 20 + i + 1}
+                  </NoBorderCell>
+                  <NoBorderCell align="right">{row.name}</NoBorderCell>
+                  <NoBorderCell align="right">
+                    <Icon name={row.symbol.toLowerCase()} size={25} />
+                  </NoBorderCell>
+                  <NoBorderCell align="right">{row.symbol}</NoBorderCell>
+                  <NoBorderCell align="right">
+                    {parseFloat(row.priceUsd).toFixed(2)}$
+                  </NoBorderCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      ) : (
+        <Box textAlign="center" mb={2}>
+          <Typography variant="h5" style={{ color: "yellow" }}>
+            Loading...
+          </Typography>
+        </Box>
+      )}
+      <Grid container justifyContent="flex-end" style={{ marginTop: "10px" }}>
+        <Pagination count={41} color="primary" onChange={handlePagination} />
+      </Grid>
     </Box>
   );
 }
